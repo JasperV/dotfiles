68c71e12fc1ffe9a50ba07ffb4eba3d2019b39a5
JasperV <jaspervermeulen1@gmail.com>
2018-12-13 13:03:42 +0100

    Update upstall.sh

--- upstall.sh
+++ upstall.sh
@@ -21,6 +21,8 @@ rm -f ~/.zcompdump; compinit
 
 sudo chmod go-w '/usr/local/share'
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 
 
 # Install dotfiles
