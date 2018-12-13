739f5a7532d2bb91e1fab186991ab27a9070c961
JasperV <jaspervermeulen1@gmail.com>
2018-12-13 13:41:57 +0100

    Update upstall.sh

--- upstall.sh
+++ upstall.sh
@@ -17,8 +17,6 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 # Install zsh and zsh-completions
 brew install zsh zsh-completions
 
-rm -f ~/.zcompdump; compinit
-
 sudo chmod go-w '/usr/local/share'
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
