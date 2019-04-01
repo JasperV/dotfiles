f97d9fb335b081c6a750e1410b9002ed4b9a6be4
JasperV <1297464+JasperV@users.noreply.github.com>
2019-04-01 15:15:08 +0200

    add franz

--- brew.sh
+++ brew.sh
@@ -80,6 +80,7 @@ brew cask install android-file-transfer
 brew cask install google-chrome
 brew cask install balenaetcher
 # brew cask install betterzip
+brew cask install franz
 brew cask install google-backup-and-sync
 brew cask install handbrake
 brew cask install iterm2
